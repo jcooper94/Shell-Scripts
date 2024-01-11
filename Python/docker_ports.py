a3e48e89a92d396af68b24766c8bf51ea5dfa7b8
Jared Cooper <152523733+autoimmunexd@users.noreply.github.com>
2024-01-11 11:47:53 -0800

    Updated docker_ports.py
    itterating over each container works and display a corresponding port.

--- Python/docker_ports.py
+++ Python/docker_ports.py
@@ -12,8 +12,14 @@ for container in containers:
   # Get the name and the ports of the container
   name = container.name
   ports = container.ports
+  #The items() method returns a view object. The view object contains the key-value pairs of the dictionary, as tuples in a list.
+  for container, port_data in ports.items():
+    if port_data:
+        first_value = next(iter(port_data), {}).get('HostPort', '').split('/')[0]
+        print(f"Container: {container} Port: {first_value}")
+    else:
+        print(f"Container: {container} Port: No port information available")
 
   # Print the name and the ports of the container
-  print(f"Container: {name}")
-  print(f"Ports: {ports}")
-  print("")
+  #print(type(ports))
+  print(f"Container: {name}" + '' + "Port: {first_value}")
