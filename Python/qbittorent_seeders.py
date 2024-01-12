21ed49c4bfe6aa08a0511792aa42d9a97598c1a5
Jared Cooper <152523733+autoimmunexd@users.noreply.github.com>
2024-01-12 18:01:37 -0500

    updated main file and combined with json test
    *thumbs up emoji*

--- Python/qbittorent_seeders.py
+++ Python/qbittorent_seeders.py
@@ -1,7 +1,7 @@
-import requests
 import shelve
-import requests
-import json
+import qbittorrentapi
+
+#implements asking the user what # of torrent seeders to choose to support a torrent by seeding.  
 
 url = 'http://192.168.1.144:8085/api/v2/torrents/info'
 
@@ -15,14 +15,18 @@ def toggle_state():
 def setup():
     ip = input('What is the local IP address of your docker qBittorrent application? ')
     port = input('What port is the qbittorrent docker container running on? ')
+    user = input('What is qBittorent username?')
+    password = input('What is qBittorent password?')
     url = f'http://{ip}:{port}/api/v2/torrents/info'
-    yes_no = input(f'You typed IP: {ip} PORT: {port}. \nIs this correct? Type Yes or No \n')
-    return yes_no.lower(), url
+    yes_no = input(f'You typed IP: {ip} PORT: {port} with a username {user} password {password}. \nIs this correct? Type Yes or No \n')
 
-def get_torrents(url):
-    torrents = requests.get(url)
-    torrents_dict = json.loads(torrents)
-    print(torrents_dict)
+    return yes_no.lower(), url, port, user, password, ip
+
+
+def get_torrents_list():
+    qbt_client = qbittorrentapi.Client(**creds)
+    for torrent in qbt_client.torrents_info():
+        print(f"{torrent.hash[-6:]}:\n{torrent.name}:\n{torrent.num_complete} total numbers of seeders in the swarm.")
 
 if __name__ == '__main__':
     #opens shelf state file
@@ -32,13 +36,22 @@ if __name__ == '__main__':
         #print(current_state)
         if current_state:
             #runs setup and returns response and newurl from it
-            response, new_url = setup()
+            response, url, port, user, password, ip = setup()
             if response == 'yes':
-                print(new_url)
+                #print(url)
+                creds = dict(host = ip, port = port, username = user, password = password)
+                credentials = shelve.open('credentials')
+                credentials['credentials'] = creds
+                credentials.close()
                 current_state = toggle_state()  # Toggle the state and update current_state
-                print(current_state)  # Print the updated state
+                #print(current_state)  # Print the updated state
             else:
-                print('Running setup again.')
+                print('Running setup.')
                 setup()
         else:
             print('Not your first time running the script, so not asking for information.')
+            elf_on_shelf = shelve.open('credentials')
+            creds = elf_on_shelf['credentials']
+            #print(creds)
+            get_torrents_list()
+            elf_on_shelf.close()
