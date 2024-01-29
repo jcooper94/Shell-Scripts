95b6cabd31d6001dfef82e542d25a58c42d105c0
Jared Cooper <152523733+autoimmunexd@users.noreply.github.com>
2024-01-29 17:51:32 -0500

    Update decimal_to_binary_class.sh

--- Bash/decimal_to_binary_class.sh
+++ Bash/decimal_to_binary_class.sh
@@ -6,15 +6,15 @@ og_num=""
 function to_binary() {
     # decimal to binary
     decimal_chart=(128 64 32 16 8 4 2 1)
+
     binary=()
 
     read -p "Input the decimal number: " decimal
+
     declare -g og_num=$decimal
 
-    for num in "${decimal_chart[@]}"
-    do
-        if (( "$decimal" >= "$num" ));
-        then
+    for num in "${decimal_chart[@]}"; do
+        if (("$decimal" >= "$num")); then
             decimal=$((decimal - num))
             binary+=(1)
         else
@@ -26,27 +26,29 @@ function to_binary() {
 }
 
 function to_class() {
-    arr=($@)  # convert all arguments into an array
+    arr=($@) # convert all arguments into an array
+
     first_four=""
-    
-    for i in {0..3}
-    do
+
+    for i in {0..3}; do
         first_four+="${arr[i]}"
     done
-    
+
     # Determine the class based on the first four digits
     case $first_four in
-        "0"*) echo "Class A";;
-        "10"*) echo "Class B";;
-        "110"*) echo "Class C";;
-        "1110"*) echo "Class D";;
-        "1111"*) echo "Class E";;
+    "0"*) echo "Class A" ;;
+    "10"*) echo "Class B" ;;
+    "110"*) echo "Class C" ;;
+    "1110"*) echo "Class D" ;;
+    "1111"*) echo "Class E" ;;
     esac
 }
 
-# call the function and set the echo to a variable and echo the variable since no return like in python 
+# call the function and set the echo to a variable and echo the variable since no return like in python
 binary=$(to_binary)
+
 class=$(to_class $binary)
 
 echo "The integer $og_num in binary is $binary."
+
 echo "It is a $class address."
