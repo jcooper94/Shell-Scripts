d1a28d38036d89e6fa0c9bd8286a1af8b88140b0
Jared Cooper <152523733+autoimmunexd@users.noreply.github.com>
2024-02-20 18:13:14 -0500

    updated pirate_box_x64

--- Bash/pirate_box.sh
+++
@@ -1,7 +0,0 @@
-#create and setup qemu arm64 on termux
-#edit preseed.cfg for termux
-
-#this script for 64bit pirate box
-#install ansible and setup docker, docker-compose
-#change /etc/motd
-#change /etc/issue
---
+++ Bash/pirate_box_x64.sh
@@ -0,0 +1,15 @@
+#arm64 like raspberry pi
+#Persistant Live USB
+
+#for devices like firestick
+#Termux installation
+
+#for repurposing old devices or vms
+#Standard 64bit ISO
+#create a package with answer file and bash startup script
+#package that into a iso
+
+#OPTIONS
+#docker, dockercompose
+#change /etc/motd
+#change /etc/issue
